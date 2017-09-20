5780dddb5cd53efe5a9e9c6e5c3ad15b6a1da6f0
marcusxdm <marcuspestana@ic.ufal.br>
2017-09-19 22:34:57 -0300

    View with Template
    Showing all products in Index

--- rightprice/views.py
+++ rightprice/views.py
@@ -3,7 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the rightprice index.")
+    product_list = Product.objects.all()
+    template = loader.get_template('rightprice/index.html')
+    context = {
+            'product_list': product_list,
+    }
+    #output = ', '.join([p.description for p in product_list])
+    return HttpResponse(template.render(context, request))
